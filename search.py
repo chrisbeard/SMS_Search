c25cf4361dc1b1756ed54a97383604eb6d694978
chrisbeard <christopher.e.beard@gmail.com>
2016-02-02 23:33:46 -0500

    Can search by keyword in message with option --keyword; exact matching performed with --exact option

--- search.py
+++ search.py
@@ -4,29 +4,58 @@ import xml.etree.ElementTree as ET
 import sys
 
 if len(sys.argv) == 1:
-    print("Usage: ./search.py XML_FILE")
+    print("Usage: ./search.py <XML_FILE> [option(s)]")
+    print()
+    print("Options:")
+    print("   --keyword    search messages by keyword")
+    print("   --exact      perform exact match")
     sys.exit(1)
 
-HEADER = '\033[95m'
-GREEN = '\033[92m'
-BLUE  = '\033[94m'
-END  = '\x1b[0m'
- 
+# Parse options
+exact_match = "--exact" in sys.argv
+keyword = None
+if "--keyword" in sys.argv:
+    keyword = input("Enter keyword to search: ")
+else:
+    name = input("Enter name to search: ")
+
+# Prepare xml file for parsing
 f = sys.argv[1]
 tree = ET.parse(f)
 root = tree.getroot()
 
-name = input("Enter name to search: ")
+# Pretty-printing
+HEADER = '\033[95m'
+GREEN = '\033[92m'
+BLUE  = '\033[94m'
+END  = '\x1b[0m'
 
+
+def print_message(child):
+    message = HEADER + "{}\n  " + END + "{}: {}{}"
+    contact = child.get('contact_name')
+    date = child.get('readable_date')
+    msg = child.get('body')
+
+    if child.get('type') == '1':
+        print(message.format(date, contact, GREEN, msg))
+    else:
+        print(message.format(date, "Me", BLUE, msg))
+
+# Print all matching messages
 for child in root:
     contact = child.get('contact_name')
-    if name in contact:
-        t = child.get('readable_date')
-        m = child.get('body')
-        # message = HEADER + "{}\n  " + END + "{}: {}{}\n"
-        message = HEADER + "{}\n  " + END + "{}: {}{}"
+    message = child.get('body')
 
-        if child.get('type') == '1':
-            print(message.format(t, contact, GREEN, m))
-        else:
-            print(message.format(t, "Me", BLUE, m))
+    if keyword:
+        if exact_match:
+            if keyword in message.split():
+                print_message(child)
+        elif keyword in message:
+                print_message(child)
+    else:
+        if exact_match:
+            if name in contact.split():
+                print_message(child)
+        elif name in contact:
+                print_message(child)
